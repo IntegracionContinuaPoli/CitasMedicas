08f3a427aabd4e157ed6562855ef136c3a3c8362
IntegracionContinuaPoli <44612028+IntegracionContinuaPoli@users.noreply.github.com>
2018-12-15 21:03:12 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -5,41 +5,54 @@
 	<meta charset="utf-8">
 		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
 		<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
-		 <link rel=stylesheet href="css/estilos.css"/>
+		<link rel=stylesheet href="css/estilos.css"/>
 	<title>OdontPG</title>
 </head>
 <body>
-    <div id="envoltura">
-        <div id="contenedor">
-	      <center><div class="tit"><h2 style="color: #000000; ">Inicio de sesión</h2>
-		  <center><div class="Ingreso">
 
-	     <table width="206" border="0" align="center" valign="middle"></table>
-		  <td width="200"><tr>
-		  <td rowspan=2>
+	<div id="envoltura">
+		<div id="contenedor">
+		<center><div class="tit"><h2 style="color: #000000; ">OdontPG</h2>
+		<center><div class="Ingreso">
+
+		<table width="206" border="0" align="center" valign="middle"></table>
+		<td width="200"><tr>
+		<td rowspan=2>
 		<form action="validar.php" method="post">
 
-		<table id="cuerpo">
+			<table id="cuerpo">
 
-		<tr><td><label style="font-size: 13.5pt"><b>Usuario: </b></label></td>
-			<td width=80> <input class="form-group has-success" style="border-radius:15px;" type="text" name="mail"></td></tr>
-		<tr><td><label style="font-size: 13.5pt"><b>Contraseña: </b></label></td>
-			<td witdh=80><input style="border-radius:15px;" type="password" name="pass"></td></tr>
-		<tr><td></td>
-			<td width=80 align=center><input class="btn btn-primary" type="submit" value="Aceptar"></td>
-			</tr></tr>
-		</table>
+				<tr>
+					<td>
+						<input class="form-group has-success" style="border-radius:10px;" type="text" name="mail" placeholder="Username">
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<input style="border-radius:10px;" type="password" name="pass" placeholder="Password">
+					</td>
+				</tr>
+				<tr>
+					<td style="text-align:center">
+						<input class="btn btn-primary" type="submit" value="Iniciar sesión">
+					</td>
+				</tr>
+			</table>
+			
 		</form>
-		
-         <br>
+
+		<br>
 
 		</td>
 		<td height="75"></tr>
 		</table>
 		</div></center></div></center>
 
-	       </div><!-- fin contenedor -->
-    </div><!--fin envoltura-->
+		</div>
+	</div>
+	
+
+	
 </body>
 
 </html>
